37de0ea6b6011b992aebfcf42ac1a7726952770c
hemel-raihan <hemelraihan1997@gmail.com>
2022-06-07 17:33:29 +0600

    test

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('login',[\App\Http\Controllers\AuthController::class, 'login']);
 Route::group([
 
-    //'middleware' => 'api',
+    'middleware' => 'api',
     'namespace' => 'App\Http\Controllers',
     //'prefix' => 'auth'
 
